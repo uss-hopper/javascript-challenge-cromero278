62c1dab51ae825fd60f95ed41d0e9d8c15635764
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-21 16:32:40 -0700

    added highlight and search/replace function

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-window.onload = function(){
-	changeColor();
-};
+ window.onload = function(){
+ 	changeColor();
+ };
 
 function changeColor() {
 	let firstParagraph = document.getElementById("change-text");
 	firstParagraph.style.color = "pink";
 }
+
+ function hilightColor() {
+	 let graphTwo = document.getElementById("upper");
+	 graphTwo.style.backgroundColor = "pink";
+ }
+
+ function hipster() {
+	let string = document.getElementById("search").innerHTML;
+	let newString = string.replace(/hipster/gi, "BYE");
+	document.getElementById("search").innerHTML = newString;
+ }
